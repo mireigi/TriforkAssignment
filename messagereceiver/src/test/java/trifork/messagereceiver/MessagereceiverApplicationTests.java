8734fe06b81adf4715adb5dda56fa19b0a0deb63
Michael Givskov <mireigi@gmail.com>
2021-10-07 15:22:14 +0200

    Added initial message receiver project

---
+++ messagereceiver/src/test/java/trifork/messagereceiver/MessagereceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package trifork.messagereceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagereceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
