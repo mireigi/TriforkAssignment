22b80c5e73f49f1600eb6e2cb9d23977fc3e2504
Michael Givskov <mireigi@gmail.com>
2021-10-11 14:24:50 +0200

    Clean up and versioning

--- messagesender/src/main/java/trifork/messagesender/Runner.java
+++ messagesender/src/main/java/trifork/messagesender/Runner.java
@@ -1,7 +1,5 @@
 package trifork.messagesender;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
